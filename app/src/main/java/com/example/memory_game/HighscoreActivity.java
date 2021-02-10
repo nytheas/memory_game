bdfa8298807f8dd31ecdb28b7db7036ec233c11e
Nytheas <nytheas@seznam.cz>
2021-02-10 18:35:56 +0100

    bckp before api changes

--- app/src/main/java/com/example/memory_game/HighscoreActivity.java
+++ app/src/main/java/com/example/memory_game/HighscoreActivity.java
@@ -15,7 +15,9 @@ public class HighscoreActivity extends AppCompatActivity {
 
     public static final String SHARED_PREFS = "SharedPrefs";
     public static final String HIGH_SCORE = "High score";
+    public static final String NAME = "Name";
     private Integer high_score;
+    private String my_name;
 
 
 
@@ -23,14 +25,29 @@ public class HighscoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highscore);
+
+        loadHighScore();
+    }
+
+    public void saveName(View v){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        TextView r = findViewById(R.id.editTextTextPersonName);
+        my_name = r.getText().toString();
+        editor.putString(NAME, (String) my_name);
+        editor.apply();
+
         loadHighScore();
     }
 
     public void loadHighScore(){
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
         high_score = sharedPreferences.getInt(HIGH_SCORE,0);
+        my_name = sharedPreferences.getString(NAME,"Anonymous");
         TextView t = findViewById(R.id.textView7);
         t.setText("High score: "+String.valueOf(high_score));
+        TextView r = findViewById(R.id.editTextTextPersonName);
+        r.setText(String.valueOf(my_name));
 
     }
 
--- server/classes.py
+++ server/classes.py
@@ -5,10 +5,16 @@ all_values = []
 
 def initialize(all_values):
    ##all_values.append({"id":3,"userId":5,"title":"lord","body":"test"})
-   all_values.append({"name":"Martin","score":6})
-   all_values.append({"name":"Franta","score":5})
-   all_values.append({"name":"Pavel","score":4})
-   all_values.append({"name":"Jana","score":3})
+   all_values.append({"name":"Martin","score":10})
+   all_values.append({"name":"Franta","score":8})
+   all_values.append({"name":"Pavel","score":6})
+   all_values.append({"name":"Jana","score":5})
+   all_values.append({"name":"Karel","score":5})
+   all_values.append({"name":"Monika","score":4})
+   all_values.append({"name":"Marie","score":4})
+   all_values.append({"name":"Petr","score":4})
+   all_values.append({"name":"Andrea","score":3})
+   all_values.append({"name":"Dalibor","score":3})
    return all_values
 
 
